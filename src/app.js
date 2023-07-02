3d4f8e1799406c8a8c4ac00fe2ebfdfb5a71aaf7
andrew-zachary <andrew_zachary@outlook.com>
2023-07-02 00:00:00 +0000

    setup express-handlebars and render home/about views, create header/footer partials

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import express from "express";
+import { create } from 'express-handlebars';
 
 const app = express();
 
+// handlebars ins
+const hbs = create({
+    extname: '.hbs',
+    helpers: {
+        loud: (str) => {
+            return str.toUpperCase()
+        }
+    }
+});
+
 // customize app
 app.use(express.static('public'));
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', './src/views');
 
 export default app;
--- src/routers/pages.js
+++ src/routers/pages.js
@@ -3,7 +3,17 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (_, res) => {
-    res.send('home page');
+    res.render('index', {
+        title: 'welcome',
+        layout: 'main'
+    });
+});
+
+router.get('/about', (_, res) => {
+    res.render('about', {
+        title: 'about',
+        layout: 'main'
+    });
 });
 
 export default router;
