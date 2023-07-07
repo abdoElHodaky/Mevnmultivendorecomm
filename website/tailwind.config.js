482c563b475df8eb5d2465f7449c37915f1bbf26
andrew-zachary <andrew_zachary@outlook.com>
2023-07-07 00:00:00 +0000

    base style for products page

---
+++ website/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.js",
+    "../server/src/views/**/*.hbs"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
