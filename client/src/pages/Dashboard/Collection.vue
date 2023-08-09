e2d80d568d6d19aac8a93bd1d62143a01ce750b3
andrew-zachary <andrew_zachary@outlook.com>
2023-08-09 00:00:00 +0000

    revisiting the dashboard collection page will not initiate fetching again

--- client/src/pages/Dashboard/Collection.vue
+++ client/src/pages/Dashboard/Collection.vue
@@ -1,32 +1,35 @@
 <script setup>
-import { onBeforeMount, ref, watch } from 'vue';
+import { onBeforeMount, onBeforeUnmount, ref, watch } from 'vue';
 import { useScroll } from '@vueuse/core';
 
 import useBaseFetch from '../../utils/fetch.js';
 import useProductStore from '../../stores/product.js';
 
-const url = ref('products/collection?page=1&limit=10');
-
 const productStore = useProductStore();
 const { arrivedState } = useScroll(window, { offset: { bottom:200 } });
+
+const url = ref(`products/collection?page=${productStore.page}&limit=10`);
 const fetch = useBaseFetch(url, {
-    afterFetch: (ctx) => productStore.productsReceived(ctx.data),
-    refetch: true
+    afterFetch: (ctx) => productStore.productsReceived(ctx.data)
 });
 
 watch(arrivedState, arrivedState => {
     if(arrivedState.bottom && productStore.hasMore && !fetch.isFetching.value) {
+        productStore.doPaginate();
         url.value = `products/collection?page=${productStore.page}&limit=10`;
+        fetch.get().json().execute();
     }
 });
 
 onBeforeMount(() => {
-    fetch.get().json().execute();
+    if(productStore.collection.length === 0) {
+        fetch.get().json().execute();
+    }
 });
 </script>
 <template>
     <ul>
-        <li v-for="item in productStore.collection" :key="item._id" class="pt-[1.25rem] px-2 hover:bg-primary-up hover:cursor-pointer">
+        <li v-for="item in productStore.collection" :key="item._id" class="pt-[1.25rem] px-2 hover:bg-primary-up">
             <div class="flex flex-col sm:flex-row items-center">
                 <div class="mb-4 sm:mb-0">
                     <img :src="`/${item.thumbnail}`" alt="" class="w-[20rem] h-[20rem] sm:w-[10rem] sm:h-[10rem] right-left-margin-1" />
--- client/src/stores/product.js
+++ client/src/stores/product.js
@@ -7,16 +7,20 @@ const useProductStore = defineStore('product', () => {
     const page = ref(1);
     const hasMore = ref(true);
 
-    const productsReceived = (products) => {
+    const productsReceived = (products) => {        
 
+        // escape in case no more items
         hasMore.value = products.length > 0;
         if(!hasMore.value) return;
 
         collection.value = collection.value.concat(products);
+    };
+
+    const doPaginate = () => {
         page.value += 1;
     };
 
-    return { collection, page, hasMore, productsReceived };
+    return { collection, page, hasMore, productsReceived, doPaginate };
 });
 
 export default useProductStore;
