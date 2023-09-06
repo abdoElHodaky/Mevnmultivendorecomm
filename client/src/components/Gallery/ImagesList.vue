8784dc635473ac40ec3eb00daa0ae14b4ad70970
andrew-zachary <andrew_zachary@outlook.com>
2023-09-06 00:00:00 +0000

    replace the loading-spinner (infinite scroll) within gallery page imageslist with showmore-btn to fix items offset after the deleting item

--- client/src/components/Gallery/ImagesList.vue
+++ client/src/components/Gallery/ImagesList.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { onBeforeMount, ref, watch } from "vue";
-import { useFetch, useScroll } from "@vueuse/core";
+import { onBeforeMount, ref } from "vue";
+import { useFetch } from "@vueuse/core";
 
 import useProductStore from "../../stores/product.js";
 
 import GalleryImagesItem from "./ImagesItem.vue";
+import LabelIconBtn from "../LabelIconBtn.vue";
 
 const productStore = useProductStore();
-const { arrivedState } = useScroll(window, { offset: { bottom:200 } });
 
 const url = ref(`${import.meta.env.VITE_API}/products/collection/images?page=${productStore.productsImagesPage}&limit=10`);
 const fetch = useFetch(url, {
@@ -16,15 +16,15 @@ const fetch = useFetch(url, {
     afterFetch: (ctx) => productStore.productsImagesReceived(ctx.data)
 });
 
-watch(arrivedState, arrivedState => {
-    if(arrivedState.bottom && productStore.hasMoreProductsImages && !fetch.isFetching.value) {
-        productStore.paginateProductsImages();
-        url.value = `${import.meta.env.VITE_API}/products/collection/images?page=${productStore.productsImagesPage}&limit=${productStore.itemsPerPage}`;
-        fetch.get().json().execute();
-    }
-});
+const loadMoreImages = () => {
+
+    productStore.paginateProductsImages();
+    url.value = `${import.meta.env.VITE_API}/products/collection/images?page=${productStore.productsImagesPage}&limit=${productStore.itemsPerPage}`;
+    fetch.get().json().execute();
+};
 
 onBeforeMount(() => {
+    
     if(productStore.productsImagesCollection.length === 0) {
         fetch.get().json().execute();
     }
@@ -33,8 +33,8 @@ onBeforeMount(() => {
 <template>
     <ul class="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-y-8 gap-x-4 justify-items-center pb-44">
         <GalleryImagesItem v-for="item in productStore.productsImagesCollection" :item="item" :key="item._id" />
-        <li v-if="productStore.hasMoreProductsImages" class="text-center py-4 overflow-hidden col-span-full">
-            <ProgressSpinner :pt="{ circle: { class: '!stroke-secondary' } }" />
+        <li v-if="productStore.hasMoreProductsImages && !fetch.isFetching.value" class="mt-8 w-full col-span-full overflow-hidden">
+            <LabelIconBtn label="show more" icon="pi pi-plus" @click="loadMoreImages" />
         </li>
     </ul>
 </template>
